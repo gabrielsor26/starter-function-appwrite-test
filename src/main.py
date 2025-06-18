77973438e680204eb413c120782f9cab250469f5
gabrielsor26 <gabriel.vilchez.chota@gmail.com>
2025-06-18 10:27:52 -0500

    test students again

--- src/main.py
+++ src/main.py
@@ -1,39 +1,15 @@
-from appwrite.client import Client
-from appwrite.services.users import Users
-from appwrite.exception import AppwriteException
-import os
 
-# This Appwrite function will be executed every time your function is triggered
+from students import get_students
+
 def main(context):
-    # You can use the Appwrite SDK to interact with other services
-    # For this example, we're using the Users service
-    client = (
-        Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
-    )
-    users = Users(client)
+    # Verificar que el método HTTP sea POST
+    if context.req.method != "POST":
+        return context.res.text("Método no permitido, use POST", status_code=405)
 
-    try:
-        response = users.list()
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log("Total users: " + str(response["total"]))
-    except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
+    # Verificar que la ruta sea /students (opcional, si usas rutas)
+    if context.req.path == "/students":
+        students = get_students()
+        return context.res.json({"students": students})
 
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
-
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+    # Si la ruta no coincide
+    return context.res.text("Ruta no encontrada", status_code=404)
