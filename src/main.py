f0975621f48704147524459ef3c7966f396f0d1a
gabrielsor26 <gabriel.vilchez.chota@gmail.com>
2025-06-18 10:23:06 -0500

    again

--- src/main.py
+++ src/main.py
@@ -1,11 +1,39 @@
-# main.py
+from appwrite.client import Client
+from appwrite.services.users import Users
+from appwrite.exception import AppwriteException
+import os
 
-from students import get_students
-
+# This Appwrite function will be executed every time your function is triggered
 def main(context):
-    if context.req.path == "/students":
-        students = get_students()
-        return context.res.json({"students": students})
-    # Puedes agregar más rutas aquí si lo necesitas
+    # You can use the Appwrite SDK to interact with other services
+    # For this example, we're using the Users service
+    client = (
+        Client()
+        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
+        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
+        .set_key(context.req.headers["x-appwrite-key"])
+    )
+    users = Users(client)
 
-    return context.res.text("Ruta no encontrada", status_code=404)
+    try:
+        response = users.list()
+        # Log messages and errors to the Appwrite Console
+        # These logs won't be seen by your end users
+        context.log("Total users: " + str(response["total"]))
+    except AppwriteException as err:
+        context.error("Could not list users: " + repr(err))
+
+    # The req object contains the request data
+    if context.req.path == "/ping":
+        # Use res object to respond with text(), json(), or binary()
+        # Don't forget to return a response!
+        return context.res.text("Pong")
+
+    return context.res.json(
+        {
+            "motto": "Build like a team of hundreds_",
+            "learn": "https://appwrite.io/docs",
+            "connect": "https://appwrite.io/discord",
+            "getInspired": "https://builtwith.appwrite.io",
+        }
+    )
